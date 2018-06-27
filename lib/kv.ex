2ef0f4854b37056a0f4a95d5969ca1e3d16707ae
vincent <vincent@gephen.co>
2018-06-27 14:47:42 +0800

    Agent + GenServer

---
+++ lib/kv.ex
@@ -0,0 +1,18 @@
+defmodule KV do
+  @moduledoc """
+  Documentation for KV.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> KV.module_name
+      KV
+
+  """
+  def module_name do
+    __MODULE__
+  end
+end
