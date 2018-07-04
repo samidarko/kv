f811111ef7d8e20380bc2752e29c496cb13d6935
vincent <vincent@gephen.co>
2018-07-04 18:15:44 +0800

    mix.exs update to fit in kv_umbrella

--- mix.exs
+++ mix.exs
@@ -2,12 +2,18 @@ defmodule KV.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :kv,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :kv,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
